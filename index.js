46796e081eaa56bb28b259fbc08099a0cc1e6ec5
pradeep <ballapradeep222@gmail.com>
2025-02-17 20:24:44 +0530

    setting up index.js file

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const adminRoutes = require('./routes/adminRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+
+
+ app.use('/admin', adminRoutes);
+ app.use('/user', userRoutes);
 
 
 
